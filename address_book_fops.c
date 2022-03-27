1f3a06a0ad6ea30b0e10be4e70e44fd36af7fbe0
Greyson Wilson <gqwilson@cpp.edu>
2022-03-27 13:11:12 -0700

    Reverted address_book_fops to previous version

--- address_book_fops.c
+++ address_book_fops.c
@@ -1,92 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include <unistd.h>
+#include <unistd.h>
 #include <sys/stat.h>
 #include <errno.h>
 #include <ctype.h>
-#include <stdbool.h>
 
 #include "address_book.h"
 
 Status load_file(AddressBook *address_book)
 {
 	int ret;
-	const int SIZE = 50;
 
-	/*
+	/* 
 	 * Check for file existance
 	 */
-	// ret = access(DEFAULT_FILE, F_OK);  // <unistd.h> on UNIX
-	struct stat buffer;
-	ret = stat(DEFAULT_FILE, &buffer);
-	;
-	if (ret == 0) // File has been detected
+	//At this point, we are assuming the address_book is empty and thus fp is NULL
+	//So really we don't need to do this stuff with address_book->fp
+	/*FILE *fileptr = fopen(DEFAULT_FILE, "r");
+	if (fileptr == NULL){
+		fclose(fileptr); //Close file, maybe return a status
+	}*/
+
+	address_book->fp = fopen(DEFAULT_FILE, "r"); //This works because we dont need to make a new ptr
+	if (address_book->fp == NULL){ //Have we found the file?
+		//throw error
+		printf("FILE NOT FOUND");
+		ret = e_fail;
+		fclose(address_book->fp);
+	}
+
+	//alternatively
+	/*if (!access(DEFAULT_FILE, F_OK))
+		printf("FILE NOT FOUND");//perror("\nFILE NOT FOUND");*/
+
+	if (ret == 0) // File has been detected, -10 would mean fail
 	{
-		/*
+		/* 
 		 * Do the neccessary step to open the file
 		 * Do error handling
-		 */
-		address_book->fp = fopen(DEFAULT_FILE, "a+");
-		if (address_book->fp == NULL) // Handle file open error
-		{
-			fprintf(stderr, "File open error(%d): %s\n", errno, strerror(errno));
-			fclose(address_book->fp);
-			return e_fail;
+		 */ 
+		int numberOfLines = 0;
+		char buffer[353];
+		//Because the file is still open we shall simply begin copying it into our list
+		//Determine how large the file is while checking for errors
+		while (fgets(buffer, 353, address_book->fp)){
+			numberOfLines++;
 		}
-		else
-		{
-			address_book->list = (ContactInfo *)calloc(SIZE, sizeof(ContactInfo));
-			FILE *ptr = fopen(DEFAULT_FILE, "r");
-			char buffer[400]; // Line size is 352 + delimiters
-			int count = 0;
-			const int FIELDS = NAME_COUNT + PHONE_NUMBER_COUNT + EMAIL_ID_COUNT;
-
-			while (fgets(buffer, sizeof(buffer), ptr))
-			{
-				int number_count = 0;
-				int email_count = 0;
-				char *line = strtok(buffer, NEXT_ENTRY);	// Line
-				char *info = strtok(line, FIELD_DELIMITER); // Info
-
-				ContactInfo contact;
-				for (int i = 0; i < FIELDS; i++)
-				{
-					if (i == 0)
-					{
-						strcpy(contact.name[0], info);
-					}
-					else if (i > 0 && i <= 5)
-					{
-						strcpy(contact.phone_numbers[number_count], info);
-						number_count++;
-					}
-					else
-					{
-						strcpy(contact.email_addresses[email_count], info);
-						email_count++;
-					}
-					info = strtok(NULL, FIELD_DELIMITER); // delimiter
-				}
-				contact.si_no = count;
-				address_book->list[count] = contact;
-				count++;
-			}
-			// Close file pointer
-			address_book->count = count;
-			fclose(ptr);
-			fclose(address_book->fp);
+		printf("%i lines read from file.\n", numberOfLines);
+		//Make an array to accomodate all lines
+		ContactInfo contactinfo[numberOfLines];
+		for (int line = 0; line < numberOfLines; line++){
+			//Put all names
+			//Put all phone numbers
+			//Put all email addresses
+			break; //DELETE ME
 		}
+
+		address_book->list = contactinfo;
+		fclose(address_book->fp);
+		
 	}
 	else
 	{
 		/* Create a file for adding entries */
+		printf("Creating new address book file, %s\n", DEFAULT_FILE);
 		address_book->fp = fopen(DEFAULT_FILE, "a");
-		address_book->count = 0;
-		address_book->list = (ContactInfo *)calloc(SIZE, sizeof(ContactInfo));
 		fclose(address_book->fp);
 	}
-
+	fclose(address_book->fp);
 	return e_success;
 }
 
@@ -95,7 +77,7 @@ Status save_file(AddressBook *address_book)
 	/*
 	 * Write contacts back to file.
 	 * Re write the complete file currently
-	 */
+	 */ 
 	address_book->fp = fopen(DEFAULT_FILE, "w");
 
 	if (address_book->fp == NULL)
@@ -103,48 +85,25 @@ Status save_file(AddressBook *address_book)
 		return e_fail;
 	}
 
-	/*
+	/* 
 	 * Add the logic to save the file
 	 * Make sure to do error handling
-	//  */
-	// // Print names
-	// fprintf(address_book->fp, "%s,", address_book->list->name);
-	// // Print phone numbers upto max allowable phone numbers
-	// int numbers = sizeof(address_book->list->phone_numbers) / NUMBER_LEN;
-	// for (int i = 0; i < numbers; i++)
-	// {
-	// 	fprintf(address_book->fp, "%s ", address_book->list->phone_numbers[i]);
-	// }
-	// // fprintf(address_book->fp, FIELD_DELIMITER);
-	// //  print emails upto max allowable emails
-	// int emails = sizeof(address_book->list->email_addresses) / EMAIL_ID_COUNT;
-	// for (int i = 0; i < emails; i++)
-	// {
-	// 	fprintf(address_book->fp, "%s ", address_book->list->email_addresses[i]);
-	// }
-	// // fprintf(address_book->fp, FIELD_DELIMITER);
-	// //  print id?
-	int count = 0;
-	int number_count = 0;
-	int email_count = 0;
-	while (count < address_book->count)
-	{
-		fprintf(address_book->fp, "%s,", address_book->list[count].name[0]);
-		number_count = 0;
-		email_count = 0;
-		while (number_count < PHONE_NUMBER_COUNT)
-		{
-			fprintf(address_book->fp, "%s,", address_book->list[count].phone_numbers[number_count]);
-			number_count++;
-		}
-		while (email_count < (EMAIL_ID_COUNT - 1))
-		{
-			fprintf(address_book->fp, "%s,", address_book->list[count].email_addresses[email_count]);
-			email_count++;
-		}
-		fprintf(address_book->fp, "%s\n", address_book->list[count].email_addresses[EMAIL_ID_COUNT - 1]);
-		count++;
+	 */ 
+	//Print names
+	fprintf(address_book->fp, "%s,", address_book->list->name);
+	//Print phone numbers upto max allowable phone numbers
+	int numbers = sizeof(address_book->list->phone_numbers) / NUMBER_LEN;
+	for (int i=0; i < numbers; i++){
+		fprintf(address_book->fp, "%s ", address_book->list->phone_numbers[i]);
 	}
+	fprintf(address_book->fp, ",");
+	//print emails upto max allowable emails
+	int emails = sizeof(address_book->list->email_addresses) / EMAIL_ID_COUNT;
+	for (int i=0; i < emails; i++){
+		fprintf(address_book->fp, "%s ", address_book->list->email_addresses[i]);
+	}
+	fprintf(address_book->fp, ",");
+	//print id?
 
 	fclose(address_book->fp);
 
