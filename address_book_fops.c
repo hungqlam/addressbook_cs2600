3f92f78850e41c43434fb497ffcf52bebc558ee7
Tran Nguyen <tranhnguyen@cpp.edu>
2022-03-27 05:40:25 -0700

    File open error

--- address_book_fops.c
+++ address_book_fops.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+//#include <unistd.h>
 #include <sys/stat.h>
 #include <errno.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 #include "address_book.h"
 
@@ -12,37 +13,35 @@ Status load_file(AddressBook *address_book)
 {
 	int ret;
 
-	/* 
+	/*
 	 * Check for file existance
 	 */
-	address_book->fp = fopen(DEFAULT_FILE, "r");
-	if (address_book->fp == NULL){
-		//throw error
-		printf("FILE NOT FOUND");
-		//perror("\nFILE NOT FOUND");
-		fclose(address_book->fp);
-		ret = -1;
-		//return e_fail;
-	}
-	fclose(address_book->fp);
-
-	//alternatively
-	if (!access(DEFAULT_FILE, F_OK))
-		printf("FILE NOT FOUND");//perror("\nFILE NOT FOUND");
-
-	if (ret == 0) // <--- What is ret?
+	// ret = access(DEFAULT_FILE, F_OK);  // <unistd.h> on UNIX
+	struct stat buffer;
+	ret = stat(DEFAULT_FILE, &buffer);
+	;
+	if (ret == 0) // File has been detected
 	{
-		/* 
+		/*
 		 * Do the neccessary step to open the file
 		 * Do error handling
-		 */ 
+		 */
+		address_book->fp = fopen(DEFAULT_FILE, "a+");
+		if (address_book->fp == NULL) //Handle file open error
+		{
+			fprintf(stderr, "File open error(%d): %s\n", errno, strerror(errno));
+			fclose(address_book->fp);
+			return e_fail;
+		}
+		else
+		{
+			
+		}
 	}
 	else
 	{
+		printf("File is Not found\n");
 		/* Create a file for adding entries */
-		printf("Creating new address book file, %s\n", DEFAULT_FILE);
-		address_book->fp = fopen(DEFAULT_FILE, "a");
-		fclose(address_book->fp);
 	}
 
 	return e_success;
@@ -53,7 +52,7 @@ Status save_file(AddressBook *address_book)
 	/*
 	 * Write contacts back to file.
 	 * Re write the complete file currently
-	 */ 
+	 */
 	address_book->fp = fopen(DEFAULT_FILE, "w");
 
 	if (address_book->fp == NULL)
@@ -61,25 +60,27 @@ Status save_file(AddressBook *address_book)
 		return e_fail;
 	}
 
-	/* 
+	/*
 	 * Add the logic to save the file
 	 * Make sure to do error handling
-	 */ 
-	//Print names
+	 */
+	// Print names
 	fprintf(address_book->fp, "%s,", address_book->list->name);
-	//Print phone numbers upto max allowable phone numbers
+	// Print phone numbers upto max allowable phone numbers
 	int numbers = sizeof(address_book->list->phone_numbers) / NUMBER_LEN;
-	for (int i=0; i < numbers; i++){
+	for (int i = 0; i < numbers; i++)
+	{
 		fprintf(address_book->fp, "%s ", address_book->list->phone_numbers[i]);
 	}
 	fprintf(address_book->fp, ",");
-	//print emails upto max allowable emails
+	// print emails upto max allowable emails
 	int emails = sizeof(address_book->list->email_addresses) / EMAIL_ID_COUNT;
-	for (int i=0; i < emails; i++){
+	for (int i = 0; i < emails; i++)
+	{
 		fprintf(address_book->fp, "%s ", address_book->list->email_addresses[i]);
 	}
 	fprintf(address_book->fp, ",");
-	//print id?
+	// print id?
 
 	fclose(address_book->fp);
 
