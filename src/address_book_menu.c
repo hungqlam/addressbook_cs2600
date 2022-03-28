5e14164d524e251c9eb13741eecce8e10fff0ad1
hungqlam <98580130+hungqlam@users.noreply.github.com>
2022-03-27 23:22:15 -0700

    edit Add Function

--- src/address_book_menu.c
+++ src/address_book_menu.c
@@ -288,7 +288,98 @@ Status menu(AddressBook *address_book)
 Status add_contacts(AddressBook *address_book)
 {
 	/* Add the functionality for adding contacts here */
+	
+	int opt;
+	int condition = 0;
+	do
+	{
+		/* code */
+		// menu_header("Add Contact:");
+		// printf("0. Back");
+		// printf("\n1. Name");
+		// printf("\n2. Phone No 1: ");
+		// printf("\n3. Email ID 1: ");
+		contact_confirmation(address_book->count, "Adding contact", address_book);
+
+		opt = get_option(NUM, "\n\nPlease select an option: ");
+		printf("Count is: %d\n", address_book->count);
+		switch (opt)
+		{
+			case e_second_opt:
+			{
+				printf("Enter the name:");
+				char str[32];
+				fflush(stdin);
+				scanf("%[^\n]s", str);
+				fflush(stdin);
+				strcpy(address_book->list[address_book->count].name[0], str);
+				condition = 1;
+				break;
+			}
+				
+			case e_third_opt:
+			{
+				int index = 0;
+				printf("Enter Phone Number index to be changed [Max %d]: ", PHONE_NUMBER_COUNT);
+				index = get_option(NUM, "");
+				index = index - 1;
+				int string_len;
+				char str[32];
+				fflush(stdin);
+				// check if the the index is negative or over the PHONE_NUMBER_COUNT
+				if (index < -1 || index > PHONE_NUMBER_COUNT)
+					break;
+
+				// Prompt the user to enter the phone number to change
+				printf("Enter Phone Number %d: ", index + 1);
+				scanf("%[^\n]s", str);
+				string_len = strlen(str) - 1;
+
+				if (str[string_len] == '\n')
+					str[string_len] = '\0'; // set the end of userInput to null
+				strcpy(address_book->list[address_book->count].phone_numbers[index], str);
+				condition = 1;
+				break;
+			}
+				
+			case e_fourth_opt:
+			{
+				int index = 0;
+				printf("Enter Email Number index to be changed [Max %d]: ", EMAIL_ID_COUNT);
+				index = get_option(NUM, "");
+				index = index - 1;
+				int string_len;
+				char str[32];
+				fflush(stdin);
+				// check if the the index is negative or over the EMAIL_ID_COUNT
+				if (index < -1 || index > EMAIL_ID_COUNT)
+					break;
+				// Prompt the user to enter the phone number to change
+				printf("Enter Email Number %d: ", index + 1);
+				scanf("%[^\n]s", str);
+				string_len = strlen(str) - 1;
+				if (str[string_len] == '\n')
+					str[string_len] = '\0'; // set the end of userInput to null
+				strcpy(address_book->list[address_book->count].email_addresses[index], str);
+				condition = 1;
+				break;
+			}
+				
+		
+			case e_first_opt:
+			// case 0
+				break;
+			default:
+				return e_fail;
+		}
+	} while (opt!=0);
+	if (condition == 1) {
+		address_book->list[address_book->count].si_no = address_book->count + 1;
+		address_book->count++;
+	}
+	return e_success;
 }
+
 
 /*(search term, pointer to book, number of contacts in list, searching name/phone/email/si_no, unused, unused)
 field: 1 = name, 2 = phone, 3 = email, 4 = serial number*/
